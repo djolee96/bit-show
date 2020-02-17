175ad3b879341e4f504d032824f40472410f3779
Your Name <you@example.com>
2020-02-18 00:28:26 +0100

    loader added

--- bit-show/src/app/components/MostPopular.jsx
+++ bit-show/src/app/components/MostPopular.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import ShowCard from "./ShowCard"
-import showsService from "../../services/showsService"
-import Show from "../../models/show"
+import ShowCard from "./ShowCard";
+import Loader from "./Loader";
+import showsService from "../../services/showsService";
+import Show from "../../models/show";
+
 class MostPopular extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            shows: []
+            shows: [],
+            isLoaded: false,
+
         }
     }
     componentDidMount() {
         showsService()
             .then(shows => {
                 shows.length = 50
-                this.setState({ shows: shows.map(show => new Show(show)) })
+                this.setState({ shows: shows.map(show => new Show(show)), isLoaded: true })
             })
     }
     render() {
         return (
             <div className="row">
-                {this.state.shows.map(show => <ShowCard showInfo={show} key={show.id} />)}
+                {this.state.isLoaded ?
+                    this.state.shows.map(show => <ShowCard showInfo={show} key={show.id} />) :
+                    <Loader />
+                }
             </div>
         )
     }
