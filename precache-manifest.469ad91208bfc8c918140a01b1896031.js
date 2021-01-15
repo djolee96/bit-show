976d5eae3b9b9e6a67c1f2c6b527902e347270cd
djolee96 <milan.491989@yahoo.com>
2021-01-15 12:49:57 +0100

    Updates

---
+++ precache-manifest.469ad91208bfc8c918140a01b1896031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fcaeb8440b65168bd5b9daf506ce2b",
+    "url": "/bit-show/bit-show/index.html"
+  },
+  {
+    "revision": "301a24e98022b3e03045",
+    "url": "/bit-show/bit-show/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "faeaa873c6bab230a580",
+    "url": "/bit-show/bit-show/static/css/main.7ba79a81.chunk.css"
+  },
+  {
+    "revision": "301a24e98022b3e03045",
+    "url": "/bit-show/bit-show/static/js/2.5f629893.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/bit-show/bit-show/static/js/2.5f629893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faeaa873c6bab230a580",
+    "url": "/bit-show/bit-show/static/js/main.80884a41.chunk.js"
+  },
+  {
+    "revision": "7991dfd6d7d15e9f8bf1",
+    "url": "/bit-show/bit-show/static/js/runtime-main.e2e6ca79.js"
+  }
+]);
